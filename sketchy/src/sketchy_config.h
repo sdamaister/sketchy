8d156f4d7307fed6d9f93c5bad9e2291e885842a
Maister <sda.maister@gmail.com>
2017-06-28 23:25:25 +0200

    sketchy_config compiling

--- sketchy/src/sketchy_config.h
+++ sketchy/src/sketchy_config.h
@@ -5,23 +5,20 @@
 #ifndef __SKETCHY_CONFIG__
 #define __SKETCHY_CONFIG__
 
-#include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
 #include <sstream>
+#include <stdio.h>
+#include <stdarg.h>
+#include <windows.h>
 
-class ConsoleLogger
+static void LogConsoleString(const char* aFormat, ...)
 {
-	static void LogConsoleString(const char* aFormat, ...)
-	{
-		va_list lArgs;
-		va_start(lArgs, aFormat);
-		char lBuff[1024] = { 0 };
-		sprintf(lBuff, aFormat, lArgs);
-		std::wostringstream os_;
-		os_ << lBuff << "\n";
-		OutputDebugString(os_.str().c_str());
-	}
+	va_list lArgs;
+	va_start(lArgs, aFormat);
+	char lBuff[1024] = { 0 };
+	sprintf(lBuff, aFormat, lArgs);
+	std::wostringstream os_;
+	os_ << lBuff << "\n";
+	OutputDebugString(os_.str().c_str());
 }
 
 #endif //__SKETCHY_CONFIG__
